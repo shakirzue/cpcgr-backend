243fb806a9b866ec5a8a59ad8031e2ff9e91caeb
shakirzue <99166577+shakirzue@users.noreply.github.com>
2022-04-07 07:16:26 -0400

    apply null check on file type col service

--- Controllers/storage.js
+++ Controllers/storage.js
@@ -10,9 +10,12 @@ const fileUploader = require('../Services/file-uploader-service');
 
 
 const blobupload = (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');  
+    res.header('Access-Control-Allow-Origin', '*');
     fileUploader.blobUpload(req, res);
-    res.json({ success: "true", message: "File Successfully Uploaded" });      
+    res.json({
+        success: "true",
+        message: "File Successfully Uploaded"
+    });
 }
 
 const getModules = (req, res, next) => {
@@ -25,13 +28,20 @@ const getModules = (req, res, next) => {
         request.input('isVisible', sql.Bit, true)
         request.query('select Id,ModuleName from dbo.File_Module WHERE isVisible = @isVisible', function (err, result) {
 
-            if (err) console.log(err)          
+            if (err) console.log(err)
             // send records as a response  
-           
+
             if (result.recordset.length > 0) {
-                return res.status(200).json({ success: true, message: "records successfully.", modules: result.recordset });
+                return res.status(200).json({
+                    success: true,
+                    message: "records successfully.",
+                    modules: result.recordset
+                });
             }
-            return res.status(500).json({ success: false, message: "incorrect information has not provided" });
+            return res.status(500).json({
+                success: false,
+                message: "incorrect information has not provided"
+            });
         });
     });
 }
@@ -47,12 +57,19 @@ const getFileTypesByModule = (req, res, next) => {
         request.query('select * from dbo.Module_File_Type WHERE ModuleId = @ModuleId', function (err, result) {
 
             if (err) console.log(err)
-         
+
             // send records as a response           
             if (result.recordset.length > 0) {
-                return res.json({ success: true, message: "records successfully.", filetypes: result.recordset });
+                return res.json({
+                    success: true,
+                    message: "records successfully.",
+                    filetypes: result.recordset
+                });
             }
-            return res.json({ success: false, message: "incorrect information has not provided" });
+            return res.json({
+                success: false,
+                message: "incorrect information has not provided"
+            });
         });
     });
 }
@@ -65,22 +82,29 @@ const getFileTypeDetailByFileTypeId = (req, res, next) => {
         // query to the database and get the records
         request.query('select mft.[FileName], ftd.FileTypeId, ftd.RequiredColumnNumber, fcd.ColumnName from dbo.[Module_File_Type] mft inner join dbo.[File_Type_Detail] ftd on mft.Id = ftd.FileTypeId inner join dbo.[FileType_Column_Detail] fcd on ftd.Id = fcd.FileDetailId', function (err, result) {
             if (err) console.log(err)
-            var allFileTypeDetails = [];           
+            var allFileTypeDetails = [];
             // send records as a response
-            req.body.FileTypeIds.split(',').forEach(element => {               
-                const finalResult = result.recordset.filter(d => d.FileTypeId.toString() === element);
-                allFileTypeDetails = allFileTypeDetails.concat(finalResult);
-            });
-         
             if (result.recordset.length > 0) {
-                return res.json({ success: true, message: "records successfully.", filedetails: allFileTypeDetails });
+                req.body.FileTypeIds.split(',').forEach(element => {
+                    const finalResult = result.recordset.filter(d => d.FileTypeId.toString() === element);
+                    allFileTypeDetails = allFileTypeDetails.concat(finalResult);
+                });
+                return res.json({
+                    success: true,
+                    message: "records successfully.",
+                    filedetails: allFileTypeDetails
+                });
             }
-            return res.json({ success: false, message: "incorrect information has not provided" });
+            return res.json({
+                success: false,
+                message: "incorrect information has not provided"
+            });
         });
     });
 }
 
-module.exports = {blobupload,
+module.exports = {
+    blobupload,
     getModules,
     getFileTypesByModule,
     getFileTypeDetailByFileTypeId
